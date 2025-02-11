abb9324394268def1d0778fe5c4953ec65db1f70
vanie0000 <124703685+vanie0000@users.noreply.github.com>
2025-02-11 15:49:55 +0530

    add logger information using morgan and winston

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 import 'dotenv/config'
 import express from 'express'
+import logger from './logger.js'
+import morgan from 'morgan'
 
 const app = express()
 const port = process.env.PORT || 3000
+const morganFormat = ':method :url :status :response-time ms'
+
+app.use(morgan(morganFormat, {
+    stream: {
+        write: (message) => {
+            const logObject = {
+                method: message.split(' ')[0],
+                url: message.split(' ')[1],
+                status: message.split(' ')[2],
+                responseTime: message.split(' ')[3],
+            }
+            logger.info(JSON.stringify(logObject))
+        }
+    }
+}))
 
 app.use(express.json())
 
